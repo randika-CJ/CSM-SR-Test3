fb74baeb81bcc5fae89cb8393f6113630ea257a8
randika-CJ <randikaprabashwara123@gmail.com>
2025-05-28 22:47:19 +0530

    Update main.js

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -6,16 +6,16 @@
 class CSMSRApp {
     constructor() {
         this.components = [
-            { id: 'header-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/header.html' },
-            { id: 'hero-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/hero.html' },
-            { id: 'research-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/research.html' },
-            { id: 'results-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/results.html' },
-            { id: 'publications-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/publications.html' },
-            { id: 'demo-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/demo.html' },
-            { id: 'downloads-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/download.html' },
-            { id: 'team-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/team.html' },
-            { id: 'contact-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/contact.html' },
-            { id: 'footer-component', file: 'https://randika-cj.github.io/CSM-SR-Test3/docs/components/footer.html' }
+            { id: 'header-component', file: 'components/header.html' },
+            { id: 'hero-component', file: 'components/hero.html' },
+            { id: 'research-component', file: 'components/research.html' },
+            { id: 'results-component', file: 'components/results.html' },
+            { id: 'publications-component', file: 'components/publications.html' },
+            { id: 'demo-component', file: 'components/demo.html' },
+            { id: 'downloads-component', file: 'components/download.html' },
+            { id: 'team-component', file: 'components/team.html' },
+            { id: 'contact-component', file: 'components/contact.html' },
+            { id: 'footer-component', file: 'components/footer.html' }
         ];
         this.loadedComponents = 0;
         this.totalComponents = this.components.length;
